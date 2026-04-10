c9fa19e9b6b5dc233260270519acd746d3cf2435
wvk5ht5vzk-blip <anders_sand93@hotmail.com>
2026-04-10 17:51:43 +0200

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,7 @@
+firebase.initializeApp({
+  apiKey:"AIzaSyDDrqLTUtImklgFF1W59aWaNolyg7GBmj0",
+  authDomain:"nesbyen-masters-v2.firebaseapp.com",
+  projectId:"nesbyen-masters-v2"
+});
+
+const db = firebase.firestore();
