9a1212410db8e6a04c84fa99e7b27cd4c9d374c6
wvk5ht5vzk-blip <anders_sand93@hotmail.com>
2026-04-12 21:49:35 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,7 +1,11 @@
-firebase.initializeApp({
-  apiKey:"AIzaSyDDrqLTUtImklgFF1W59aWaNolyg7GBmj0",
-  authDomain:"nesbyen-masters-v2.firebaseapp.com",
-  projectId:"nesbyen-masters-v2"
-});
+const firebaseConfig = {
+  apiKey: "AIzaSyDDrqLTUImklgFF1W59aWaNo1yg7GBmjo",
+  authDomain: "nesbyen-masters-v2.firebaseapp.com",
+  projectId: "nesbyen-masters-v2",
+  messagingSenderId: "733141987995",
+  appId: "1:733141987995:web:XXXXX" // <-- hvis du har den, hvis ikke går det fint uten
+};
+
+firebase.initializeApp(firebaseConfig);
 
 const db = firebase.firestore();
