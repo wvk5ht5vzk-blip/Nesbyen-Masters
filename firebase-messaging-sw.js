a96e455a88b2bc7462ef9b5639b83bf936f70170
wvk5ht5vzk-blip <anders_sand93@hotmail.com>
2026-04-12 21:07:04 +0200

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,13 +2,12 @@ importScripts("https://www.gstatic.com/firebasejs/10.12.0/firebase-app-compat.js
 importScripts("https://www.gstatic.com/firebasejs/10.12.0/firebase-messaging-compat.js");
 
 firebase.initializeApp({
-  apiKey: "DIN_API_KEY",
-  authDomain: "DIN_APP.firebaseapp.com",
-  projectId: "DIN_PROJECT_ID",
-  messagingSenderId: "733141987995",
-  appId: "DIN_APP_ID"
+  apiKey:"AIzaSyDDrqLTUtImklgFF1W59aWaNolyg7GBmj0",
+  authDomain:"nesbyen-masters-v2.firebaseapp.com",
+  projectId:"nesbyen-masters-v2"
 });
 
+const db = firebase.firestore();
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage(function(payload) {
