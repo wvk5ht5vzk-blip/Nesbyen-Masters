79e2914b218b2018d5df02c69c6e1978c3223d5e
wvk5ht5vzk-blip <anders_sand93@hotmail.com>
2026-05-28 16:27:17 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1974,10 +1974,8 @@ if(state.screen === "chat"){
    const chatBottom = document.getElementById("chatBottom");
 
 if(chatBottom){
-  chatBottom.scrollIntoView({
-    behavior: "smooth"
-  });
-} 
+  chatBottom.scrollIntoView(false);
+}
 
   }, 50);
 }
